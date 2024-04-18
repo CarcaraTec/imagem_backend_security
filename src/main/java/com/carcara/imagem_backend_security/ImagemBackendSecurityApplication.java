a5e5daf10178f1253f32ac3cc47676929637d4f2
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2024-04-18 20:35:37 -0300

    IM-36/feature-Infra da projeto back-end, security

---
+++ src/main/java/com/carcara/imagem_backend_security/ImagemBackendSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.carcara.imagem_backend_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagemBackendSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagemBackendSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carcara/imagem_backend_security/ImagemBackendSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carcara.imagem_backend_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagemBackendSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
