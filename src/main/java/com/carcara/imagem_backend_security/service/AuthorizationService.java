d6b51446ad0993aacce63bfa49a078ca03480a37
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2024-04-29 22:33:31 -0300

    IM-55 Criando o authorization service

---
+++ src/main/java/com/carcara/imagem_backend_security/service/AuthorizationService.java
@@ -0,0 +1,19 @@
+package com.carcara.imagem_backend_security.service;
+
+import com.carcara.imagem_backend_security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username);
+    }
+}
