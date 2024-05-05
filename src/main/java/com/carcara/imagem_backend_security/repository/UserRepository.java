f6cbea7dc60bea56c8c6501283d583deab7a7485
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2024-05-05 14:07:39 -0300

    IM-68 End-point para retornar os dados do usuário

--- src/main/java/com/carcara/imagem_backend_security/repository/UserRepository.java
+++ src/main/java/com/carcara/imagem_backend_security/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.carcara.imagem_backend_security.repository;
 
 import com.carcara.imagem_backend_security.model.User;
+import com.carcara.imagem_backend_security.repository.projection.DadosUsuarioProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,12 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     UserDetails findByUsername(String username);
+
+    @Query(value = "SELECT " +
+            " username AS username, " +
+            " email AS email, " +
+            " cpf AS cpf " +
+            " FROM users " +
+            " WHERE cpf = :cpf ", nativeQuery = true)
+    DadosUsuarioProjection getDadosUsuario(@Param("cpf") String cpf);
 }
---
+++ src/main/java/com/carcara/imagem_backend_security/repository/projection/DadosUsuarioProjection.java
@@ -0,0 +1,9 @@
+package com.carcara.imagem_backend_security.repository.projection;
+
+public interface DadosUsuarioProjection {
+
+    String getUSername();
+    String getEmail();
+    String getCpf();
+
+}
