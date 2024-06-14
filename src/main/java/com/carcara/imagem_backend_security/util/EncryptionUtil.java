02d7b0c5b616946a6f24928b537639cec1aff02f
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2024-06-13 21:32:22 -0300

    IM-141 criando método decrypt

--- src/main/java/com/carcara/imagem_backend_security/util/EncryptionUtil.java
+++ src/main/java/com/carcara/imagem_backend_security/util/EncryptionUtil.java
@@ -3,6 +3,7 @@ package com.carcara.imagem_backend_security.util;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import java.util.Base64;
 
 public class EncryptionUtil {
@@ -15,6 +16,14 @@ public class EncryptionUtil {
         cipher.init(Cipher.ENCRYPT_MODE, key);
         byte[] encryptedBytes = cipher.doFinal(input.getBytes());
         return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String input, SecretKey key, byte[] iv) throws Exception {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, key, ivParameterSpec);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(input));
+        return new String(decryptedBytes);
     }
 
     public static SecretKey generateKey() throws Exception {
